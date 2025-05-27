6743fe03af7ed8d0c2fb5a3d12806b107b0cf8bb
petargeorgiev18 <petar01920192@gmail.com>
2025-05-27 14:57:08 +0300

    Added services and interfaces for the services - Business logic and Core layer made

---
+++ TVShow_27.05/TVShow.Core/Services/ContestantService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TVShow.Core.Intefaces;
+using TVShow.Data.Models;
+using TVShow.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TVShow.Core.Services
+{
+    public class ContestantService : IContestantService
+    {
+        private readonly TVShowDbContext context;
+        public ContestantService(TVShowDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Contestant>> GetAllAsync()
+        {
+            return await context.Contestants
+                .Include(c => c.ShowContestants)
+                .ToListAsync();
+        }
+        public async Task<Contestant?> GetByIdAsync(int id)
+        {
+            return await context.Contestants
+                .Include(c => c.ShowContestants)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+        public async Task AddAsync(Contestant contestant)
+        {
+            context.Contestants.Add(contestant);
+            await context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Contestant contestant)
+        {
+            context.Contestants.Update(contestant);
+            await context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var contestant = await context.Contestants.FirstOrDefaultAsync(c=>c.Id==id);
+            if (contestant != null)
+            {
+                context.Contestants.Remove(contestant);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ TVShow_27.05/TVShow.Core/Services/ShowService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TVShow.Core.Intefaces;
+using TVShow.Data.Models;
+using TVShow.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TVShow.Core.Services
+{
+    public class ShowService : IShowService
+    {
+        private readonly TVShowDbContext context;
+        public ShowService(TVShowDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Show>> GetAllAsync()
+        {
+            return await context.Shows
+                .Include(s => s.Quizzes)
+                .Include(s => s.ShowContestants)
+                .ToListAsync();
+        }
+        public async Task<Show?> GetByIdAsync(int id)
+        {
+            return await context.Shows
+                .Include(s => s.Quizzes)
+                .Include(s => s.ShowContestants)
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+        public async Task AddAsync(Show show)
+        {
+            context.Shows.Add(show);
+            await context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Show show)
+        {
+            context.Shows.Update(show);
+            await context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var show = await context.Shows.FirstOrDefaultAsync(s=>s.Id == id);
+            if (show != null)
+            {
+                context.Shows.Remove(show);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
