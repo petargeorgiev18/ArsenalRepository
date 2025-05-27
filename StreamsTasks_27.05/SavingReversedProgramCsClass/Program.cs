e122a9cc83721bdea6b35e9c87fab3855f7c35dc
petargeorgiev18 <petar01920192@gmail.com>
2025-05-27 12:59:41 +0300

    Some stream tasks added

---
+++ StreamsTasks_27.05/SavingReversedProgramCsClass/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SavingReversedProgramCsClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Program.cs"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../Program.txt"))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        char[] lineReversed = line.Reverse().ToArray();
+                        for (int i = 0; i < lineReversed.Length; i++)
+                        {
+                            writer.Write($"{lineReversed[i]}");
+                        }
+                        writer.WriteLine();
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
