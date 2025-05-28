df7cc25115d744c99f31f5cfba4ee51fed30588a
Your Name <your_email@example.com>
2025-05-29 01:31:15 +0300

    Tasks for streams completed

---
+++ StreamsTasks_27.05/Encryption/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+
+namespace Encryption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] key;
+            byte[] iv;
+            using (FileStream readStream = new FileStream("../../../textFile.txt", FileMode.Open))
+            {
+                using (FileStream writeStream = new FileStream("../../../encryptedTextFile.enc", FileMode.Create))
+                {
+                    using (Aes aes = Aes.Create())
+                    {
+                        key = aes.Key;
+                        iv = aes.IV;
+                        ICryptoTransform encryptor = aes.CreateEncryptor();
+                        using (CryptoStream cryptoStream = new CryptoStream(writeStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            readStream.CopyTo(cryptoStream);
+                        }
+                    }
+                }
+            }
+            using (FileStream decryptedStream = new FileStream("../../../decryptedTextFile.txt", FileMode.Create, FileAccess.Write))
+            {
+                using (FileStream encryptedStream = new FileStream("../../../encryptedTextFile.enc", FileMode.Open, FileAccess.Read))
+                {
+                    using (Aes aes = Aes.Create())
+                    {
+                        aes.Key = key;
+                        aes.IV = iv;
+                        ICryptoTransform decryptor = aes.CreateDecryptor();
+                        using (CryptoStream cryptoStream = new CryptoStream(encryptedStream, decryptor, CryptoStreamMode.Read))
+                        {
+                            cryptoStream.CopyTo(decryptedStream);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StreamsTasks_27.05/WordFrequency/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace WordFrequency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Console.ReadLine();
+            string filePath = $"../../../{fileName}.txt";
+            if (File.Exists(filePath))
+            {
+                Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] lineWords = line.Split(new char[] { ' ', ',', '.', '!', '?' }
+                                ,StringSplitOptions.RemoveEmptyEntries);
+                        foreach (string word in lineWords)
+                        {
+                            string wordLower = word.ToLower();
+                            if (!wordFrequency.ContainsKey(wordLower))
+                            {
+                                wordFrequency[wordLower] = 0;
+                            }
+                            wordFrequency[wordLower]++;
+                        }
+                    }
+                }
+                using (StreamWriter writer = new StreamWriter($"../../../Result.txt", append: false))
+                {
+                    foreach (var pair in wordFrequency)
+                    {
+                        writer.WriteLine($"{pair.Key} -> {pair.Value} puti");
+                    }
+                }
+            }
+        }
+    }
+}
