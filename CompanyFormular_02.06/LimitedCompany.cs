a22eca761a9a90ca000057937856f2b75d53f1f6
petargeorgiev18 <petar01920192@gmail.com>
2025-06-02 14:53:28 +0300

    The classes for the company formular made

---
+++ CompanyFormular_02.06/LimitedCompany.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CompanyFormular_02._06
+{
+    public class LimitedCompany : Company
+    {
+        private string registrationNumber;
+        private double currentCapital;
+
+        private const double InitialCapital = 2.0;
+
+        public string RegistrationNumber
+        {
+            get => registrationNumber;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Номерът на учредителния акт не може да е празен.");
+                registrationNumber = value;
+            }
+        }
+
+        public double CurrentCapital
+        {
+            get => currentCapital;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Актуалният капитал не може да е отрицателен.");
+                }
+                currentCapital = value;
+            }
+        }
+
+        public LimitedCompany(string name, string establishmentDate, string bulstat,
+                              string registrationNumber, double currentCapital)
+            : base(name, establishmentDate, bulstat)
+        {
+            RegistrationNumber = registrationNumber;
+            CurrentCapital = currentCapital;
+        }
+
+        public double CalculateIncomeTax()
+        {
+            double profit = CurrentCapital - InitialCapital;
+            return profit > 0 ? profit * 0.10 : 0;
+        }
+    }
+}
---
+++ CompanyFormular_02.06/SoleTrader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CompanyFormular_02._06
+{
+    public class SoleTrader : Company
+    {
+        private string ownerName;
+        private double initialCapital;
+        private double currentCapital;
+
+        public string OwnerName
+        {
+            get => ownerName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Името на собственика не може да е празно.");
+                }
+                ownerName = value;
+            }
+        }
+
+        public double InitialCapital
+        {
+            get => initialCapital;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Първоначалният капитал не може да е отрицателен.");
+                }
+                initialCapital = value;
+            }
+        }
+
+        public double CurrentCapital
+        {
+            get => currentCapital;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Актуалният капитал не може да е отрицателен.");
+                }
+                currentCapital = value;
+            }
+        }
+
+        public SoleTrader(string name, string establishmentDate, string bulstat,
+                          string ownerName, double initialCapital, double currentCapital)
+            : base(name, establishmentDate, bulstat)
+        {
+            OwnerName = ownerName;
+            InitialCapital = initialCapital;
+            CurrentCapital = currentCapital;
+        }
+
+        public double CalculateIncomeTax()
+        {
+            double profit = CurrentCapital - InitialCapital;
+            return profit > 0 ? profit * 0.15 : 0;
+        }
+    }
+}
