1e92d5a675760cb9f2835da4379a631b86e0111d
petargeorgiev18 <petar01920192@gmail.com>
2025-12-12 08:34:42 +0200

    Final changes to Presents Project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+            //int maxLength = 1;
+            int index = 0;
+            int lastIndex = 0;
+            int number = 0;
+            int currentMaxLength = 0;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] == list[i + 1])
+                {
+                    currentMaxLength++;
+                    index = i;
+                    lastIndex = i + 1;
+                    number = list[i];
+                }
+                currentMaxLength = 1;
+            }
+            for (int i = index; i <= lastIndex; i++)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
